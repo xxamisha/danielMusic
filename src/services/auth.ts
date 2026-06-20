d37959f07c8cccf1db79ac55d272dde1b7abbb58
xxamisha <amisha.lalazeel@gmail.com>
2026-06-20 19:17:48 +1200

    changed auth

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
-const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI || 'https://danielsmusic.netlify.app/';
+const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI ;
 
 const SCOPES = [
   "user-read-playback-state",
